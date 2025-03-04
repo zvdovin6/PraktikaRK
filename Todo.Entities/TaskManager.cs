32d7d44a51fd896f539a09f1e25f72803d2981a4
ArtOvchinnickov <User@DESKTOP-0JI8E8N>
2025-03-04 22:25:40 +0300

    Изменения 5ю40

---
+++ Todo.Entities/TaskManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Entities
+{
+    public class TaskManager
+    {
+        private static TaskManager _instance;
+        public static TaskManager Instance => _instance ??= new TaskManager();
+
+        public ObservableCollection<TaskModel> AllTasks { get; private set; }
+
+        private TaskManager()
+        {
+            AllTasks = new ObservableCollection<TaskModel>();
+        }
+    }
+
+}
